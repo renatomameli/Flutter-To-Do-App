16d3b2b937e2b3b5b8f16e5a4944ce90e0c8c639
renato.mameli <renato.mameli@de.rhenus.com>
2022-03-26 10:46:50 +0100

    Added new screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,46 +1,50 @@
 import 'package:flutter/material.dart';
-import 'package:habit_changer/AddHabitRoute.dart';
-import 'package:habit_changer/body.dart';
+
+import 'AddHabitRoute.dart';
 import 'HabitStorage.dart';
+import 'body.dart';
 import 'nav_bar.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(const MaterialApp(
+    title: 'Habits',
+    home: MainRoute(),
+  ));
+}
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class MainRoute extends StatelessWidget {
+  const MainRoute({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        title: 'Habits',
-        home: Scaffold(
-          appBar: AppBar(
-            title: Text("Habits"),
-            backgroundColor: Colors.black38,
-            leading: GestureDetector(
-              onTap: () {
-                openNavBar();
-              },
-              child: Icon(
-                Icons.menu, // add custom icons also
-              ),
-            ),
-            actions: <Widget>[
-              Padding(
-                  padding: EdgeInsets.only(right: 20.0),
-                  child: GestureDetector(
-                    onTap: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) => const AddHabitRoute()),
-                      );
-                    },
-                    child: Icon(Icons.add),
-                    ),
-                  )
-            ],
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Habits'),
+        backgroundColor: Colors.black38,
+        leading: GestureDetector(
+          onTap: () {
+            openNavBar();
+          },
+          child: Icon(
+            Icons.menu, // add custom icons also
           ),
-          body: HabitBody(HabitStorage()),
-        ));
+        ),
+        actions: <Widget>[
+          Padding(
+            padding: EdgeInsets.only(right: 20.0),
+            child: GestureDetector(
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const AddHabitRoute()),
+                );
+              },
+              child: Icon(Icons.add),
+            ),
+          )
+        ],
+      ),
+      body: HabitBody(HabitStorage()),
+    );
   }
 }
