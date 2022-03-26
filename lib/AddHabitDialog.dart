a8175da7107fe920d70e6061fa793c8eba283295
renato.mameli <renato.mameli@de.rhenus.com>
2022-03-26 12:26:29 +0100

    Extract Dialog in a seperate class

---
+++ lib/AddHabitDialog.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+import 'AddHabitRoute.dart';
+
+class AddHabitDialog {
+  Widget buildPopupDialog(BuildContext context) {
+    final ButtonStyle style =
+    ElevatedButton.styleFrom(
+      shadowColor: Colors.black38,
+      textStyle: const TextStyle(fontSize: 20),
+    );
+
+    return new AlertDialog(
+      title: const Center(
+          child: Text("What is your goal?")
+      ),
+      content: new Column(
+        mainAxisSize: MainAxisSize.min,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          ElevatedButton(
+            style: style,
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => const AddHabitRoute()),
+              );},
+            child: Center(child: const Text('Build a new habit')),),
+          ElevatedButton(
+            style: style,
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => const AddHabitRoute()),
+              );},
+            child: Center(child: const Text('Lose or substitute a habit')),),
+        ],
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:habit_changer/AddHabitDialog.dart';
 
-import 'AddHabitRoute.dart';
 import 'Constants.dart';
 import 'HabitStorage.dart';
 import 'body.dart';
@@ -37,7 +37,7 @@ class MainRoute extends StatelessWidget {
               onTap: () {
                 showDialog(
                   context: context,
-                  builder: (BuildContext context) => _buildPopupDialog(context),
+                  builder: (BuildContext context) => AddHabitDialog().buildPopupDialog(context),
                 );
               },
               child: Icon(Icons.add),
@@ -50,38 +50,3 @@ class MainRoute extends StatelessWidget {
   }
 }
 
-Widget _buildPopupDialog(BuildContext context) {
-  final ButtonStyle style =
-  ElevatedButton.styleFrom(
-    shadowColor: Colors.black38,
-    textStyle: const TextStyle(fontSize: 20),
-  );
-
-  return new AlertDialog(
-    title: const Center(
-        child: Text("What is your goal?")
-    ),
-    content: new Column(
-      mainAxisSize: MainAxisSize.min,
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: <Widget>[
-        ElevatedButton(
-          style: style,
-          onPressed: () {
-            Navigator.push(
-            context,
-            MaterialPageRoute(builder: (context) => const AddHabitRoute()),
-          );},
-          child: Center(child: const Text('Build a new habit')),),
-        ElevatedButton(
-          style: style,
-          onPressed: () {
-            Navigator.push(
-            context,
-            MaterialPageRoute(builder: (context) => const AddHabitRoute()),
-          );},
-          child: Center(child: const Text('Lose or substitute a habit')),),
-      ],
-    ),
-  );
-}
